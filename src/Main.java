a9c5e0148070fc295ba5066b21322578789377b9
Evgeny Nikishov <evgenynikishov@mail.ru>
2022-09-11 23:01:09 +0300

    findProduct added

--- src/Main.java
+++ src/Main.java
@@ -34,6 +34,9 @@ public class Main {
                 case 3:
                     removeProduct();
                     break;
+                case 4:
+                    findProduct();
+                    break;
                 default:
                     System.out.println("Такого номера операции не существует");
             }
@@ -64,6 +67,20 @@ public class Main {
         } catch (NumberFormatException e) {
             listProducts.remove(product);
             showProduct();
+        }
+    }
+
+    static void findProduct() {
+        System.out.println("Введите продукт для поиска");
+        String product = scanner.nextLine();
+        int countMatches = 0;
+        for (int i = 0; i < listProducts.size(); i++) {
+            if (listProducts.get(i).toLowerCase().contains(product.toLowerCase())) {
+                System.out.println(listProducts.get(i));
+            }
+        }
+        if (countMatches == 0) {
+            System.out.println("Ничего не найдено");
         }
     }
 }
